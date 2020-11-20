4d0d1208d8222e0242d937511a67e5b3d8186c8b
Danny <dannynguyen312@gmail.com>
2020-11-21 00:42:48 +1100

    Reverted back to the original code from commands

--- commands.py
+++ commands.py
@@ -3,10 +3,6 @@ from flask import Blueprint
 
 db_commands = Blueprint("db", __name__)
 
-@db_commands.cli.command("richardsWisdom")
-def widsom():
-    print('hello world')
-
 @db_commands.cli.command("create")
 def create_db():
     db.create_all()
